c7e8cf698d71d9d092f782b4ca1814af206a099f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-05-25 22:14:12 +0200

    Update AbstractSpriteSourceProvider.java

--- Forge/src/main/java/fuzs/puzzleslib/api/data/v1/AbstractSpriteSourceProvider.java
+++ Forge/src/main/java/fuzs/puzzleslib/api/data/v1/AbstractSpriteSourceProvider.java
@@ -6,10 +6,15 @@ import net.minecraftforge.common.data.SpriteSourceProvider;
 
 public abstract class AbstractSpriteSourceProvider extends SpriteSourceProvider {
 
-    public AbstractSpriteSourceProvider(PackOutput packOutput, String modId, ExistingFileHelper fileHelper) {
-        super(packOutput, fileHelper, modId);
+    public AbstractSpriteSourceProvider(PackOutput packOutput, ExistingFileHelper fileHelper) {
+        super(packOutput, fileHelper, "");
     }
 
     @Override
     protected abstract void addSources();
+
+    @Override
+    public String getName() {
+        return "Sprite Sources";
+    }
 }
