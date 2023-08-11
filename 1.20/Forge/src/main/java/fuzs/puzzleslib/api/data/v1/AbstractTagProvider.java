75705db70045c7cd05eaec32bdbc5db7a6c4c5d4
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-08-11 15:44:49 +0200

    add TagProviderExtension

--- 1.20/Forge/src/main/java/fuzs/puzzleslib/api/data/v1/AbstractTagProvider.java
+++ 1.20/Forge/src/main/java/fuzs/puzzleslib/api/data/v1/AbstractTagProvider.java
@@ -8,11 +8,15 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.*;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -22,7 +26,7 @@ import java.util.concurrent.ExecutionException;
 
 public final class AbstractTagProvider {
 
-    public abstract static class Blocks extends BlockTagsProvider {
+    public abstract static class Blocks extends BlockTagsProvider implements TagProviderExtension<Block, IntrinsicHolderTagsProvider.IntrinsicTagAppender<Block>> {
 
         public Blocks(GatherDataEvent evt, String modId) {
             this(evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -39,9 +43,19 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<Block>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<Block> tag(TagKey<Block> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
-    public abstract static class Fluids extends FluidTagsProvider {
+    public abstract static class Fluids extends FluidTagsProvider implements TagProviderExtension<Fluid, IntrinsicHolderTagsProvider.IntrinsicTagAppender<Fluid>> {
 
         public Fluids(GatherDataEvent evt, String modId) {
             this(evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -58,9 +72,19 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<Fluid>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<Fluid> tag(TagKey<Fluid> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
-    public abstract static class Items extends ItemTagsProvider {
+    public abstract static class Items extends ItemTagsProvider implements TagProviderExtension<Item, IntrinsicHolderTagsProvider.IntrinsicTagAppender<Item>> {
 
         public Items(GatherDataEvent evt, String modId) {
             this(evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -96,9 +120,19 @@ public final class AbstractTagProvider {
         protected void copy(TagKey<Block> blockTagKey, TagKey<Item> itemTagKey) {
             throw new UnsupportedOperationException();
         }
+
+        @Override
+        public ResourceKey<? extends Registry<Item>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<Item> tag(TagKey<Item> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
-    public abstract static class EntityTypes extends EntityTypeTagsProvider {
+    public abstract static class EntityTypes extends EntityTypeTagsProvider implements TagProviderExtension<EntityType<?>, IntrinsicHolderTagsProvider.IntrinsicTagAppender<EntityType<?>>> {
 
         public EntityTypes(GatherDataEvent evt, String modId) {
             this(evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -115,9 +149,19 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<EntityType<?>>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<EntityType<?>> tag(TagKey<EntityType<?>> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
-    public abstract static class GameEvents extends GameEventTagsProvider {
+    public abstract static class GameEvents extends GameEventTagsProvider implements TagProviderExtension<GameEvent, IntrinsicHolderTagsProvider.IntrinsicTagAppender<GameEvent>> {
 
         public GameEvents(GatherDataEvent evt, String modId) {
             this(evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -134,6 +178,16 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<GameEvent>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<GameEvent> tag(TagKey<GameEvent> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
     @Deprecated(forRemoval = true)
@@ -152,7 +206,7 @@ public final class AbstractTagProvider {
         }
     }
 
-    public abstract static class Simple<T> extends TagsProvider<T> {
+    public abstract static class Simple<T> extends TagsProvider<T> implements TagProviderExtension<T, TagsProvider.TagAppender<T>> {
 
         public Simple(ResourceKey<? extends Registry<T>> registryKey, GatherDataEvent evt, String modId) {
             this(registryKey, evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -169,9 +223,19 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<T>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public TagAppender<T> tag(TagKey<T> tagKey) {
+            return super.tag(tagKey);
+        }
     }
 
-    public abstract static class Intrinsic<T> extends IntrinsicHolderTagsProvider<T> {
+    public abstract static class Intrinsic<T> extends IntrinsicHolderTagsProvider<T> implements TagProviderExtension<T, IntrinsicHolderTagsProvider.IntrinsicTagAppender<T>> {
 
         public Intrinsic(ResourceKey<? extends Registry<T>> registryKey, GatherDataEvent evt, String modId) {
             this(registryKey, evt.getGenerator().getPackOutput(), evt.getExistingFileHelper(), modId, evt.getLookupProvider());
@@ -201,5 +265,30 @@ public final class AbstractTagProvider {
 
         @Override
         protected abstract void addTags(HolderLookup.Provider provider);
+
+        @Override
+        public ResourceKey<? extends Registry<T>> registryKey() {
+            return this.registryKey;
+        }
+
+        @Override
+        public IntrinsicTagAppender<T> tag(TagKey<T> tagKey) {
+            return super.tag(tagKey);
+        }
+    }
+
+    public interface TagProviderExtension<T, A extends TagsProvider.TagAppender<T>> {
+
+        ResourceKey<? extends Registry<T>> registryKey();
+
+        A tag(TagKey<T> tagKey);
+
+        default A tag(String resourceLocation) {
+            return this.tag(new ResourceLocation(resourceLocation));
+        }
+
+        default A tag(ResourceLocation resourceLocation) {
+            return this.tag(TagKey.create(this.registryKey(), resourceLocation));
+        }
     }
 }
