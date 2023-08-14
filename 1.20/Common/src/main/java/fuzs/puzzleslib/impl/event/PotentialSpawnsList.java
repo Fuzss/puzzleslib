9513aa7c07d622aeaedabeaa7498500460a78291
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-08-14 16:18:30 +0200

    improve PotentialSpawnsList

--- 1.20/Common/src/main/java/fuzs/puzzleslib/impl/event/PotentialSpawnsList.java
+++ 1.20/Common/src/main/java/fuzs/puzzleslib/impl/event/PotentialSpawnsList.java
@@ -2,14 +2,16 @@ package fuzs.puzzleslib.impl.event;
 
 import java.util.AbstractList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
 import java.util.function.Predicate;
 
 public final class PotentialSpawnsList<E> extends AbstractList<E> {
     private final List<E> list;
     private final Predicate<E> add;
-    private final Predicate<Object> remove;
+    private final Predicate<E> remove;
 
-    public PotentialSpawnsList(List<E> list, Predicate<E> add, Predicate<Object> remove) {
+    public PotentialSpawnsList(List<E> list, Predicate<E> add, Predicate<E> remove) {
         this.list = list;
         this.add = add;
         this.remove = remove;
@@ -18,6 +20,39 @@ public final class PotentialSpawnsList<E> extends AbstractList<E> {
     @Override
     public E get(int index) {
         return this.list.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Objects.checkIndex(index, this.size());
+        E e = this.remove(index);
+        this.add(index, element);
+        return e;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        Objects.checkIndex(index, this.size() + 1);
+        index = this.size() - index;
+        Stack<E> stack = new Stack<>();
+        while (index-- > 0) {
+            stack.push(this.remove(this.size() - 1));
+        }
+        this.add(element);
+        while (!stack.isEmpty()) {
+            this.add(stack.pop());
+        }
+    }
+
+    @Override
+    public E remove(int index) {
+        Objects.checkIndex(index, this.size());
+        E e = this.get(index);
+        if (this.remove(e)) {
+            return e;
+        } else {
+            throw new IllegalStateException();
+        }
     }
 
     @Override
@@ -30,8 +65,9 @@ public final class PotentialSpawnsList<E> extends AbstractList<E> {
         return this.add.test(e);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public boolean remove(Object o) {
-        return this.remove.test(o);
+        return this.remove.test((E) o);
     }
 }
