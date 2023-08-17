5494a7a50cd973ce0d76f28a2730fa377fa03645
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-08-17 15:17:18 +0200

    new container screen helpers

--- 1.20/Common/src/main/java/fuzs/puzzleslib/api/client/screen/v2/ScreenHelper.java
+++ 1.20/Common/src/main/java/fuzs/puzzleslib/api/client/screen/v2/ScreenHelper.java
@@ -94,4 +94,52 @@ public interface ScreenHelper {
         Objects.requireNonNull(minecraft, "minecraft is null");
         return (int) (minecraft.mouseHandler.ypos() * minecraft.getWindow().getGuiScaledHeight() / minecraft.getWindow().getScreenHeight());
     }
+
+    /**
+     * Iterates of all slots owned by a container to find if any of them is hovered.
+     *
+     * @param screen the current container screen to source slots from
+     * @param mouseX mouse x position
+     * @param mouseY mouse y position
+     * @return the active slot or null
+     */
+    @Nullable
+    default Slot findSlot(AbstractContainerScreen<?> screen, double mouseX, double mouseY) {
+        for (Slot slot : screen.getMenu().slots) {
+            if (slot.isActive() && this.isHovering(screen, slot, mouseX, mouseY)) {
+                return slot;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the mouse cursor is hovering over a given slot.
+     *
+     * @param screen the current container screen owning the slot
+     * @param slot   the slot to check if hovered
+     * @param mouseX mouse x position
+     * @param mouseY mouse y position
+     * @return is the mouse cursor hovering the slot
+     */
+    default boolean isHovering(AbstractContainerScreen<?> screen, Slot slot, double mouseX, double mouseY) {
+        mouseX -= this.getLeftPos(screen);
+        mouseY -= this.getTopPos(screen);
+        return this.isHovering(slot.x - 1, slot.y - 1, 18, 18, mouseX, mouseY);
+    }
+
+    /**
+     * Checks if the mouse cursor is hovering a defined region.
+     *
+     * @param posX   the x-start of the hover area
+     * @param posY   the y-start of the hover area
+     * @param width  the width from x-start of the hover area
+     * @param height the height from y-start of the hover area
+     * @param mouseX mouse x position
+     * @param mouseY mouse y position
+     * @return is the mouse cursor hovering the defined region
+     */
+    default boolean isHovering(int posX, int posY, int width, int height, double mouseX, double mouseY) {
+        return mouseX >= posX && mouseX < posX + width && mouseY >= posY && mouseY < posY + height;
+    }
 }
