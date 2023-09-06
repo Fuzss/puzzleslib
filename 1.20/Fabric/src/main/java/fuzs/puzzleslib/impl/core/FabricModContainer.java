9570e7a9061610512af3107d64029ea5e9363161
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-09-06 15:40:23 +0200

    remove redundant mod container methods

--- 1.20/Fabric/src/main/java/fuzs/puzzleslib/impl/core/FabricModContainer.java
+++ 1.20/Fabric/src/main/java/fuzs/puzzleslib/impl/core/FabricModContainer.java
@@ -1,14 +1,11 @@
 package fuzs.puzzleslib.impl.core;
 
 import fuzs.puzzleslib.api.core.v1.ModContainer;
-import net.fabricmc.loader.api.metadata.ModDependency;
-import net.fabricmc.loader.api.metadata.ModEnvironment;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.fabricmc.loader.api.metadata.Person;
 
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -59,40 +56,7 @@ public record FabricModContainer(net.fabricmc.loader.api.ModContainer container,
     }
 
     @Override
-    public Optional<String> getIconPath(int size) {
-        return this.metadata.getIconPath(size);
-    }
-
-    @Override
-    public boolean isClientOnly() {
-        return this.metadata.getEnvironment() == ModEnvironment.CLIENT;
-    }
-
-    @Override
-    public boolean isLibrary() {
-        // Fabric does not provide a way to mark mods as libraries, this is as close as it gets
-        return this.metadata.containsCustomValue("fabric-loom:generated") && this.metadata.getCustomValue("fabric-loom:generated").getAsBoolean();
-    }
-
-    @Override
     public Optional<Path> findResource(String... path) {
         return this.container.findPath(String.join("/", path));
-    }
-
-    @Override
-    public List<String> getDependencyIds() {
-        return this.metadata.getDependencies().stream().filter(dependency -> dependency.getKind().isPositive() && !dependency.getKind().isSoft()).map(ModDependency::getModId).toList();
-    }
-
-    @Override
-    public Optional<String> getUpdateUrl() {
-        // Fabric has no built-in update checker, third-party mods such as Mod Menu can provide such functionality
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean isUpdateAvailable() {
-        // Fabric has no built-in update checker, third-party mods such as Mod Menu can provide such functionality
-        return false;
     }
 }
