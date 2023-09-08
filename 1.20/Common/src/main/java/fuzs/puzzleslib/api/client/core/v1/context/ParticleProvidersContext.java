1cd13b7b84c088c5afea28662ab537ed2016126e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-09-08 13:41:25 +0200

    Update ParticleProvidersContext.java

--- 1.20/Common/src/main/java/fuzs/puzzleslib/api/client/core/v1/context/ParticleProvidersContext.java
+++ 1.20/Common/src/main/java/fuzs/puzzleslib/api/client/core/v1/context/ParticleProvidersContext.java
@@ -56,9 +56,9 @@ public interface ParticleProvidersContext {
      * Register a client-side factory for a particle type that has not been registered in common and will not be present on dedicated servers.
      * <p>Intended for client-only mods to allow for custom particles, which would otherwise be unavailable on dedicated servers if registered normally due to registry sync (at least on Forge).
      *
-     * @param type     common particle type
-     * @param provider particle factory
-     * @param <T>      type of particle
+     * @param identifier       client particle type identifier
+     * @param particleProvider particle factory
+     * @param <T>              type of particle
      */
     default <T extends ParticleOptions> void registerClientParticleProvider(ResourceLocation identifier, ParticleProvider<T> particleProvider) {
         ClientParticleTypesImpl.INSTANCE.getParticleTypesManager(identifier.getNamespace()).register(identifier, particleProvider);
@@ -68,7 +68,7 @@ public interface ParticleProvidersContext {
      * Register a client-side factory for a particle type that has not been registered in common and will not be present on dedicated servers.
      * <p>Intended for client-only mods to allow for custom particles, which would otherwise be unavailable on dedicated servers if registered normally due to registry sync (at least on Forge).
      *
-     * @param particleType     common particle type
+     * @param identifier       client particle type identifier
      * @param particleProvider particle factory
      * @param <T>              type of particle
      */
@@ -80,9 +80,9 @@ public interface ParticleProvidersContext {
      * Register a client-side sprite based factory for a particle type that has not been registered in common and will not be present on dedicated servers.
      * <p>Intended for client-only mods to allow for custom particles, which would otherwise be unavailable on dedicated servers if registered normally due to registry sync (at least on Forge).
      *
-     * @param type    common particle type
-     * @param factory particle factory
-     * @param <T>     type of particle
+     * @param identifier      client particle type identifier
+     * @param particleFactory particle factory
+     * @param <T>             type of particle
      */
     default <T extends ParticleOptions> void registerClientParticleProvider(ResourceLocation identifier, ParticleEngine.SpriteParticleRegistration<T> particleFactory) {
         ClientParticleTypesImpl.INSTANCE.getParticleTypesManager(identifier.getNamespace()).register(identifier, particleFactory);
