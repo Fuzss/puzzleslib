af76201799b19e2109140b1d395709fac483622f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-09-29 00:30:57 +0200

    add proper forge data provider factory

--- 1.20/Forge/src/main/java/fuzs/puzzleslib/api/data/v2/core/DataProviderHelper.java
+++ 1.20/Forge/src/main/java/fuzs/puzzleslib/api/data/v2/core/DataProviderHelper.java
@@ -9,13 +9,22 @@ import net.minecraftforge.data.event.GatherDataEvent;
 
 import java.util.Objects;
 
+/**
+ * A helper class for registering {@link DataProvider} instances which run during data-gen in a development environment.
+ */
 public final class DataProviderHelper {
 
     private DataProviderHelper() {
 
     }
 
-    public static void registerDataProviders(String modId, DataProviderContext.Factory... factories) {
+    /**
+     * Registers factories for multiple {@link DataProvider} instances to be run during data-gen (when {@link GatherDataEvent} fires).
+     *
+     * @param modId     the current mod id
+     * @param factories all data provider factories to run
+     */
+    public static void registerDataProviders(String modId, ForgeDataProviderContext.Factory... factories) {
         if (!ModLoaderEnvironment.INSTANCE.isDevelopmentEnvironment()) return;
         Objects.checkIndex(0, factories.length);
         ModContainerHelper.findModEventBus(modId).ifPresent(eventBus -> {
@@ -25,11 +34,18 @@ public final class DataProviderHelper {
         });
     }
 
-    static void onGatherData(GatherDataEvent evt, String modId, DataProviderContext.Factory... factories) {
+    /**
+     * Implementation of the actual {@link GatherDataEvent}.
+     *
+     * @param evt       the data-gen event
+     * @param modId     the current mod id
+     * @param factories all data provider factories to run
+     */
+    static void onGatherData(GatherDataEvent evt, String modId, ForgeDataProviderContext.Factory... factories) {
         Objects.checkIndex(0, factories.length);
-        DataProviderContext context = ForgeDataProviderContext.fromEvent(modId, evt);
+        ForgeDataProviderContext context = ForgeDataProviderContext.fromEvent(modId, evt);
         DataGenerator dataGenerator = evt.getGenerator();
-        for (DataProviderContext.Factory factory : factories) {
+        for (ForgeDataProviderContext.Factory factory : factories) {
             DataProvider dataProvider = factory.apply(context);
             if (dataProvider instanceof ExistingFileHelperHolder holder) {
                 holder.puzzleslib$setExistingFileHelper(evt.getExistingFileHelper());
