0d07556b6923b3fad47dbeaa1957a3db19f7e329
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-11 16:28:27 +0100

    prepare 1.20.4 port

---
+++ Common/src/main/java/fuzs/puzzleslib/impl/event/SpawnTypeMob.java
@@ -0,0 +1,8 @@
+package fuzs.puzzleslib.impl.event;
+
+import net.minecraft.world.entity.MobSpawnType;
+
+public interface SpawnTypeMob {
+
+    MobSpawnType puzzleslib$getSpawnType();
+}
---
+++ Forge/src/main/java/fuzs/puzzleslib/core/package-info.java
@@ -0,0 +1,3 @@
+@Deprecated(forRemoval = true)
+package fuzs.puzzleslib.core;
+
