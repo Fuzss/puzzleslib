0192f957b3a1388f4e9f6c7998877b044b8e2eb4
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-30 10:16:36 +0100

    Update BonemealCallback.java

--- Common/src/main/java/fuzs/puzzleslib/api/event/v1/entity/player/BonemealCallback.java
+++ Common/src/main/java/fuzs/puzzleslib/api/event/v1/entity/player/BonemealCallback.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
+// TODO rename to UseBoneMealCallback::onUseBoneMeal
 @FunctionalInterface
 public interface BonemealCallback {
     EventInvoker<BonemealCallback> EVENT = EventInvoker.lookup(BonemealCallback.class);
@@ -15,13 +16,14 @@ public interface BonemealCallback {
      * Called when a bone meal is used on a block by the player, a dispenser, or a farmer villager.
      * <p>Useful for adding custom bone meal behavior to blocks, or for cancelling vanilla interactions.
      *
-     * @param level level bone meal event occurs in
-     * @param pos position bone meal is applied to
-     * @param block block state bone meal is applied to
-     * @param stack the bone meal stack
-     * @return  {@link EventResult#PASS} to continue with vanilla,
-     *          {@link EventResult#ALLOW} to set as handled and let vanilla show particles + arm swing animation,
-     *          {@link EventResult#DENY} to not do anything
+     * @param level      level bone meal event occurs in
+     * @param blockPos   position bone meal is applied to
+     * @param blockState block state bone meal is applied to
+     * @param itemStack  the bone meal stack
+     *
+     * @return {@link EventResult#PASS} to continue with vanilla,
+     * {@link EventResult#ALLOW} to set as handled and let vanilla show particles + arm swing animation,
+     * {@link EventResult#DENY} to not do anything
      */
-    EventResult onBonemeal(Level level, BlockPos pos, BlockState block, ItemStack stack);
+    EventResult onBonemeal(Level level, BlockPos blockPos, BlockState blockState, ItemStack itemStack);
 }
