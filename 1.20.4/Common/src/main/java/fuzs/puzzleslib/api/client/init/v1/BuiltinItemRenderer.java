d5aa130ff641e08391490e418c937647e436f808
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-13 00:30:26 +0100

    Update BuiltinItemRenderer.java

--- 1.20.4/Common/src/main/java/fuzs/puzzleslib/api/client/init/v1/BuiltinItemRenderer.java
+++ 1.20.4/Common/src/main/java/fuzs/puzzleslib/api/client/init/v1/BuiltinItemRenderer.java
@@ -6,7 +6,8 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 /**
- * Provides {@link net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer#renderByItem} as a functional interface.
+ * Provides {@link net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer#renderByItem} as a functional
+ * interface.
  */
 @FunctionalInterface
 public interface BuiltinItemRenderer {
@@ -15,12 +16,13 @@ public interface BuiltinItemRenderer {
      * Renders an item stack.
      * <p>Kindly copied from Fabric Api :)
      *
-     * @param stack             the rendered item stack
-     * @param mode              the model transformation mode
-     * @param matrices          the matrix stack
-     * @param vertexConsumers   the vertex consumer provider
-     * @param light             packed light map coordinates
-     * @param overlay           the overlay UV passed to {@link com.mojang.blaze3d.vertex.VertexConsumer#overlayCoords}
+     * @param itemStack          the rendered item stack
+     * @param itemDisplayContext the model transformation mode
+     * @param poseStack          the matrix stack
+     * @param multiBufferSource  the vertex consumer provider
+     * @param packedLight        packed light map coordinates
+     * @param packedOverlay      the overlay UV passed to
+     *                           {@link com.mojang.blaze3d.vertex.VertexConsumer#overlayCoords}
      */
-    void renderByItem(ItemStack stack, ItemDisplayContext mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay);
+    void renderByItem(ItemStack itemStack, ItemDisplayContext itemDisplayContext, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, int packedOverlay);
 }
