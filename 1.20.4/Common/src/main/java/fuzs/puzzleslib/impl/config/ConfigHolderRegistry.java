58227505c4cda8e78b49cee3717d7313921ffc72
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-22 11:49:10 +0100

    switch @Deprecated to @ApiStatus.Internal

--- 1.20.4/Common/src/main/java/fuzs/puzzleslib/impl/config/ConfigHolderRegistry.java
+++ 1.20.4/Common/src/main/java/fuzs/puzzleslib/impl/config/ConfigHolderRegistry.java
@@ -3,14 +3,15 @@ package fuzs.puzzleslib.impl.config;
 import fuzs.puzzleslib.api.config.v3.ConfigCore;
 import fuzs.puzzleslib.api.config.v3.ConfigDataHolder;
 import fuzs.puzzleslib.api.config.v3.ConfigHolder;
+import org.jetbrains.annotations.ApiStatus;
 
 public interface ConfigHolderRegistry extends ConfigHolder {
 
-    @Deprecated
+    @ApiStatus.Internal
     @Override
     <T extends ConfigCore> ConfigDataHolder<T> getHolder(Class<T> clazz);
 
-    @Deprecated
+    @ApiStatus.Internal
     @Override
     default <T extends ConfigCore> T get(Class<T> clazz) {
         return ConfigHolder.super.get(clazz);
