4250d129c3c964cfb7fd8c295a46e1b06479074f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-06-07 16:28:32 +0200

    ComponentHelper::toString

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v20.4.48-1.20.4] - 2024-06-07
+### Added
+- Add `ComponentHelper::toString`
+
 ## [v20.4.47-1.20.4] - 2024-06-07
 ### Added
 - Add `ComputeFieldOfViewCallback`
--- Common/src/main/java/fuzs/puzzleslib/api/chat/v1/ComponentHelper.java
+++ Common/src/main/java/fuzs/puzzleslib/api/chat/v1/ComponentHelper.java
@@ -6,8 +6,10 @@ import net.minecraft.network.chat.FormattedText;
 import net.minecraft.util.FormattedCharSequence;
 
 /**
- * A helper class for converting various text representations used by vanilla to {@link Component}.
- * <p>Useful for text instances returned from {@link net.minecraft.client.StringSplitter} and {@link Component#getVisualOrderText()}.
+ * A helper class for converting various text representations.
+ * <p>
+ * Useful for text instances returned from {@link net.minecraft.client.StringSplitter} and
+ * {@link Component#getVisualOrderText()}.
  */
 public class ComponentHelper {
 
@@ -29,5 +31,35 @@ public class ComponentHelper {
      */
     public static Component toComponent(FormattedCharSequence formattedCharSequence) {
         return new FormattedContentSink(formattedCharSequence).getComponent();
+    }
+
+    /**
+     * Converts an instance of {@link FormattedText} to a string which includes formatting codes supplied via configured
+     * {@link net.minecraft.network.chat.Style Styles}.
+     * <p>
+     * This is mostly useful when working with instances where vanilla still renders raw strings, which inherently
+     * support the old chat formatting system, such as in
+     * {@link net.minecraft.client.gui.components.DebugScreenOverlay}.
+     *
+     * @param formattedText the text to convert
+     * @return the string
+     */
+    public static String toString(FormattedText formattedText) {
+        return new FormattedContentSink(formattedText).getString();
+    }
+
+    /**
+     * Converts an instance of {@link FormattedCharSequence} to a string which includes formatting codes supplied via
+     * configured {@link net.minecraft.network.chat.Style Styles}.
+     * <p>
+     * This is mostly useful when working with instances where vanilla still renders raw strings, which inherently
+     * support the old chat formatting system, such as in
+     * {@link net.minecraft.client.gui.components.DebugScreenOverlay}.
+     *
+     * @param formattedCharSequence the text to convert
+     * @return the string
+     */
+    public static String toString(FormattedCharSequence formattedCharSequence) {
+        return new FormattedContentSink(formattedCharSequence).getString();
     }
 }
