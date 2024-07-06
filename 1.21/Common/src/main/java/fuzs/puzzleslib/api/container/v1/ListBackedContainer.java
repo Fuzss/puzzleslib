69f1e68904b2c1ee486850e676e2b1c0241a3aeb
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-07-07 00:32:33 +0200

    rename ContainerImpl

--- 1.21/Common/src/main/java/fuzs/puzzleslib/api/container/v1/ListBackedContainer.java
+++ 1.21/Common/src/main/java/fuzs/puzzleslib/api/container/v1/ListBackedContainer.java
@@ -5,40 +5,44 @@ import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 
 /**
  * A simple {@link Container} implementation with only default methods and an item list getter.
  */
 @FunctionalInterface
-public interface ContainerImpl extends Container {
+public interface ListBackedContainer extends Container {
 
     /**
      * Creates a container backed by the item list.
      */
-    static ContainerImpl of(NonNullList<ItemStack> items) {
+    static ListBackedContainer of(NonNullList<ItemStack> items) {
         return () -> items;
     }
 
     /**
      * Creates a new empty container with the specified size.
      */
-    static ContainerImpl of(int size) {
+    static ListBackedContainer of(int size) {
         return of(NonNullList.withSize(size, ItemStack.EMPTY));
     }
 
     /**
      * Retrieves the item list backing this inventory.
+     * <p>
+     * Must not be called <code>getItems</code> as it clashes with {@link BaseContainerBlockEntity#getItems()} causing
+     * remapping issues.
      */
-    NonNullList<ItemStack> getItems();
+    NonNullList<ItemStack> getContainerItems();
 
     @Override
     default int getContainerSize() {
-        return this.getItems().size();
+        return this.getContainerItems().size();
     }
 
     @Override
     default boolean isEmpty() {
-        for (ItemStack stack : this.getItems()) {
+        for (ItemStack stack : this.getContainerItems()) {
             if (!stack.isEmpty()) {
                 return false;
             }
@@ -48,25 +52,25 @@ public interface ContainerImpl extends Container {
 
     @Override
     default ItemStack getItem(int slot) {
-        return slot >= 0 && slot < this.getContainerSize() ? this.getItems().get(slot) : ItemStack.EMPTY;
+        return slot >= 0 && slot < this.getContainerSize() ? this.getContainerItems().get(slot) : ItemStack.EMPTY;
     }
 
     @Override
     default ItemStack removeItem(int slot, int count) {
-        ItemStack result = ContainerHelper.removeItem(this.getItems(), slot, count);
+        ItemStack result = ContainerHelper.removeItem(this.getContainerItems(), slot, count);
         if (!result.isEmpty()) this.setChanged();
         return result;
     }
 
     @Override
     default ItemStack removeItemNoUpdate(int slot) {
-        return ContainerHelper.takeItem(this.getItems(), slot);
+        return ContainerHelper.takeItem(this.getContainerItems(), slot);
     }
 
     @Override
     default void setItem(int slot, ItemStack stack) {
         if (slot >= 0 && slot < this.getContainerSize()) {
-            this.getItems().set(slot, stack);
+            this.getContainerItems().set(slot, stack);
             if (!stack.isEmpty() && stack.getCount() > this.getMaxStackSize()) {
                 stack.setCount(this.getMaxStackSize());
             }
@@ -76,7 +80,7 @@ public interface ContainerImpl extends Container {
 
     @Override
     default void clearContent() {
-        this.getItems().clear();
+        this.getContainerItems().clear();
         this.setChanged();
     }
 
