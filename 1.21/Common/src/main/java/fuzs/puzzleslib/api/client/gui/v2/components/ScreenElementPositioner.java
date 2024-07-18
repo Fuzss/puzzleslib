8fface9fccd345e7a22392655d1b6876b52d458f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-07-18 14:59:53 +0200

    add TooltipBuilder

--- 1.21/Common/src/main/java/fuzs/puzzleslib/api/client/gui/v2/components/ScreenElementPositioner.java
+++ 1.21/Common/src/main/java/fuzs/puzzleslib/api/client/gui/v2/components/ScreenElementPositioner.java
@@ -11,12 +11,17 @@ import java.util.List;
 
 /**
  * A small helper class for positioning a new {@link AbstractWidget} on a screen next to an already existing widget.
- * <p>The implementation is able to locate the existing widget as indicated by provided translation keys,
- * and then only positions the new widget if it does not overlap with another already existing widget.
- * <p>This class is mainly intended for placing {@link net.minecraft.client.gui.components.ImageButton} instances on {@link net.minecraft.client.gui.screens.TitleScreen}
- * and {@link net.minecraft.client.gui.screens.PauseScreen}, as it's rather likely for other mods to interfere, introducing a necessity to check for possible overlap.
- * <p>Also when using this class during init events it is recommended to run the init events during a phase later than the normal event phase,
- * to possibly run after other mods have added their own widgets, so they can be taken into account when checking for overlap.
+ * <p>
+ * The implementation is able to locate the existing widget as indicated by provided translation keys, and then only
+ * positions the new widget if it does not overlap with another already existing widget.
+ * <p>
+ * This class is mainly intended for placing {@link net.minecraft.client.gui.components.ImageButton} instances on
+ * {@link net.minecraft.client.gui.screens.TitleScreen} and {@link net.minecraft.client.gui.screens.PauseScreen}, as
+ * it's rather likely for other mods to interfere, introducing a necessity to check for possible overlap.
+ * <p>
+ * Also, when using this class during init events it is recommended to run the init events during a phase later than the
+ * normal event phase, to possibly run after other mods have added their own widgets, so they can be taken into account
+ * when checking for overlap.
  */
 public final class ScreenElementPositioner {
 
@@ -25,42 +30,71 @@ public final class ScreenElementPositioner {
     }
 
     /**
-     * Attempts to position <code>element</code> next to other <code>widgets</code>, located via the translation keys provided in <code>translationKeys</code>.
-     * <p>When successful the element position has already been changed, all that's left to the caller is actually adding the element to the current screen.
+     * Attempts to position element next to other widgets, located via the provided translation keys.
+     * <p>
+     * When successful the element position has already been changed, all that's left to the caller is actually adding
+     * the element to the current screen.
      *
      * @param element         the element to position next to another element already present on the screen
      * @param widgets         a view of all widgets on the current screen
-     * @param translationKeys the translation keys to use for locating other widgets to place <code>element</code> next to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that for widgets without a displayed {@link net.minecraft.network.chat.Component} (such as {@link net.minecraft.client.gui.components.ImageButton}) the narration key usually works, too
-     * @return was positioning <code>element</code> successful; the element position has already been changed, all that's left to the caller is actually adding the element to the current screen; otherwise if positioning has failed <code>element</code> is unchanged
+     * @param translationKeys the translation keys to use for locating other widgets to place <code>element</code> next
+     *                        to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that for widgets
+     *                        without a displayed {@link net.minecraft.network.chat.Component} (such as
+     *                        {@link net.minecraft.client.gui.components.ImageButton}) the narration key usually works,
+     *                        too
+     * @return was positioning <code>element</code> successful; the element position has already been changed, all
+     *         that's left to the caller is actually adding the element to the current screen; otherwise if positioning
+     *         has failed <code>element</code> is unchanged
      */
     public static boolean tryPositionElement(LayoutElement element, List<? extends GuiEventListener> widgets, String... translationKeys) {
         return tryPositionElement(element, widgets, false, translationKeys);
     }
 
     /**
-     * Attempts to position <code>element</code> next to other <code>widgets</code>, located via the translation keys provided in <code>translationKeys</code>.
-     * <p>When successful the element position has already been changed, all that's left to the caller is actually adding the element to the current screen.
+     * Attempts to position element next to other widgets, located via the provided translation keys.
+     * <p>
+     * When successful the element position has already been changed, all that's left to the caller is actually adding
+     * the element to the current screen.
      *
-     * @param element            the element to position next to another element already present on the screen
-     * @param widgets            a view of all widgets on the current screen
-     * @param tryPositionRightFirst by default <code>element</code> will be tried to be placed on the left side of a found other element, only if that does not work placing on the right side is attempted; this option inverts that and tries to place to the right side first
-     * @param translationKeys    the translation keys to use for locating other widgets to place <code>element</code> next to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that for widgets without a displayed {@link net.minecraft.network.chat.Component} (such as {@link net.minecraft.client.gui.components.ImageButton}) the narration key usually works, too
-     * @return was positioning <code>element</code> successful; the element position has already been changed, all that's left to the caller is actually adding the element to the current screen; otherwise if positioning has failed <code>element</code> is unchanged
+     * @param element               the element to position next to another element already present on the screen
+     * @param widgets               a view of all widgets on the current screen
+     * @param tryPositionRightFirst by default <code>element</code> will be tried to be placed on the left side of a
+     *                              found other element, only if that does not work placing on the right side is
+     *                              attempted; this option inverts that and tries to place to the right side first
+     * @param translationKeys       the translation keys to use for locating other widgets to place <code>element</code>
+     *                              next to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that
+     *                              for widgets without a displayed {@link net.minecraft.network.chat.Component} (such
+     *                              as {@link net.minecraft.client.gui.components.ImageButton}) the narration key
+     *                              usually works, too
+     * @return was positioning <code>element</code> successful; the element position has already been changed, all
+     *         that's left to the caller is actually adding the element to the current screen; otherwise if positioning
+     *         has failed <code>element</code> is unchanged
      */
     public static boolean tryPositionElement(LayoutElement element, List<? extends GuiEventListener> widgets, boolean tryPositionRightFirst, String... translationKeys) {
         return tryPositionElement(element, widgets, tryPositionRightFirst, 4, translationKeys);
     }
 
     /**
-     * Attempts to position <code>element</code> next to other <code>widgets</code>, located via the translation keys provided in <code>translationKeys</code>.
-     * <p>When successful the element position has already been changed, all that's left to the caller is actually adding the element to the current screen.
+     * Attempts to position element next to other widgets, located via the provided translation keys.
+     * <p>
+     * When successful the element position has already been changed, all that's left to the caller is actually adding
+     * the element to the current screen.
      *
-     * @param element            the element to position next to another element already present on the screen
-     * @param widgets            a view of all widgets on the current screen
-     * @param tryPositionRightFirst by default <code>element</code> will be tried to be placed on the left side of a found other element, only if that does not work placing on the right side is attempted; this option inverts that and tries to place to the right side first
-     * @param horizontalOffset   the horizontal distance between <code>element</code> and the other element it is placed next to
-     * @param translationKeys    the translation keys to use for locating other widgets to place <code>element</code> next to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that for widgets without a displayed {@link net.minecraft.network.chat.Component} (such as {@link net.minecraft.client.gui.components.ImageButton}) the narration key usually works, too
-     * @return was positioning <code>element</code> successful; the element position has already been changed, all that's left to the caller is actually adding the element to the current screen; otherwise if positioning has failed <code>element</code> is unchanged
+     * @param element               the element to position next to another element already present on the screen
+     * @param widgets               a view of all widgets on the current screen
+     * @param tryPositionRightFirst by default <code>element</code> will be tried to be placed on the left side of a
+     *                              found other element, only if that does not work placing on the right side is
+     *                              attempted; this option inverts that and tries to place to the right side first
+     * @param horizontalOffset      the horizontal distance between <code>element</code> and the other element it is
+     *                              placed next to
+     * @param translationKeys       the translation keys to use for locating other widgets to place <code>element</code>
+     *                              next to, the key is retrieved from {@link AbstractWidget#getMessage()}; note that
+     *                              for widgets without a displayed {@link net.minecraft.network.chat.Component} (such
+     *                              as {@link net.minecraft.client.gui.components.ImageButton}) the narration key
+     *                              usually works, too
+     * @return was positioning <code>element</code> successful; the element position has already been changed, all
+     *         that's left to the caller is actually adding the element to the current screen; otherwise if positioning
+     *         has failed <code>element</code> is unchanged
      */
     public static boolean tryPositionElement(LayoutElement element, List<? extends GuiEventListener> widgets, boolean tryPositionRightFirst, int horizontalOffset, String... translationKeys) {
         final int originalX = element.getX();
@@ -79,6 +113,7 @@ public final class ScreenElementPositioner {
                 }
             }
         }
+
         // reset if we were not successful in finding a position next to another widget
         element.setPosition(originalX, originalY);
         return false;
@@ -108,6 +143,7 @@ public final class ScreenElementPositioner {
                 }
             }
         }
+
         return true;
     }
 
