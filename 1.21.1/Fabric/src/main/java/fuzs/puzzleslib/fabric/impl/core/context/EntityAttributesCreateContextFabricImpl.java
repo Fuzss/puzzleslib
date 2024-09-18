44d3c58ed3cc617898a4aefc0148a7e75fac0b05
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-09-18 22:36:32 +0200

    Update EntityAttributesCreateContextFabricImpl.java

--- 1.21.1/Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/context/EntityAttributesCreateContextFabricImpl.java
+++ 1.21.1/Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/context/EntityAttributesCreateContextFabricImpl.java
@@ -5,16 +5,12 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRe
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.DefaultAttributes;
 
 public final class EntityAttributesCreateContextFabricImpl implements EntityAttributesCreateContext {
 
     @Override
     public void registerEntityAttributes(EntityType<? extends LivingEntity> entityType, AttributeSupplier.Builder builder) {
-        // this is not allowed on Forge, use the separate method which mirrors the Forge implementation
-        if (DefaultAttributes.hasSupplier(entityType)) {
-            throw new IllegalStateException("Duplicate DefaultAttributes entry: " + entityType);
-        }
+        // do not check DefaultAttributes::hasSupplier, it crashes on some dedicated servers
         FabricDefaultAttributeRegistry.register(entityType, builder);
     }
 }
