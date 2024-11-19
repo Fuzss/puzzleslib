a1432641c3d99821dab43a5065e47416681964fa
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-11-19 16:54:13 +0100

    fix LivingChangeTargetCallback on fabric

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v21.3.3-1.21.3] - 2024-11-19
+### Added
+- Add `NativeImageHelper`
+### Fixed
+- Fix `LivingChangeTargetCallback` implementation on Fabric
+
 ## [v21.3.2-1.21.3] - 2024-11-19
 ### Fixed
 - Update `GuiGraphicsHelper` to account for removed `blitOffset` parameter in `GuiGraphics`
--- Common/src/main/java/fuzs/puzzleslib/api/event/v1/entity/living/LivingChangeTargetCallback.java
+++ Common/src/main/java/fuzs/puzzleslib/api/event/v1/entity/living/LivingChangeTargetCallback.java
@@ -12,12 +12,12 @@ public interface LivingChangeTargetCallback {
     /**
      * Called when a {@link net.minecraft.world.entity.Mob} sets a new target.
      *
-     * @param entity the entity setting a new target
-     * @param target the target to be set, can be <code>null</code>
+     * @param livingEntity the entity setting a new target
+     * @param target       the target to be set, can be <code>null</code>
      * @return <ul>
      *         <li>{@link EventResult#INTERRUPT INTERRUPT} to prevent the target from changing</li>
      *         <li>{@link EventResult#PASS PASS} to allow the target to change to the set value</li>
      *         </ul>
      */
-    EventResult onLivingChangeTarget(LivingEntity entity, DefaultedValue<LivingEntity> target);
+    EventResult onLivingChangeTarget(LivingEntity livingEntity, DefaultedValue<LivingEntity> target);
 }
