39e1281ce4c79e0945b4c5832f4e69204d66db50
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-11-27 22:41:59 +0000

    overhaul AddDataPackReloadListenersCallback

--- Common/src/main/java/fuzs/puzzleslib/api/event/v1/server/AddDataPackReloadListenersCallback.java
+++ Common/src/main/java/fuzs/puzzleslib/api/event/v1/server/AddDataPackReloadListenersCallback.java
@@ -2,28 +2,30 @@ package fuzs.puzzleslib.api.event.v1.server;
 
 import fuzs.puzzleslib.api.event.v1.core.EventInvoker;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.ReloadableServerRegistries;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 
 import java.util.function.BiConsumer;
-import java.util.function.Function;
 
 @FunctionalInterface
 public interface AddDataPackReloadListenersCallback {
-    EventInvoker<AddDataPackReloadListenersCallback> EVENT = EventInvoker.lookup(
-            AddDataPackReloadListenersCallback.class);
+    EventInvoker<AddDataPackReloadListenersCallback> EVENT = EventInvoker.lookup(AddDataPackReloadListenersCallback.class);
 
     /**
      * Adds a listener to the server resource manager (for data packs) to reload at the end of all resources.
-     * <p>
-     * Note that the incoming registries provider might not have full access to all registries.
      *
-     * @param consumer registers a reload listener with an id for debugging, common listener types include:
-     *                 <ul>
-     *                 <li>{@link net.minecraft.server.packs.resources.PreparableReloadListener}</li>
-     *                 <li>{@link net.minecraft.server.packs.resources.ResourceManagerReloadListener}</li>
-     *                 <li>{@link net.minecraft.server.packs.resources.SimplePreparableReloadListener}</li>
-     *                 </ul>
+     * @param fullRegistries        the registries from {@link ReloadableServerRegistries.LoadResult#layers()}
+     * @param lookupWithUpdatedTags the registries from
+     *                              {@link ReloadableServerRegistries.LoadResult#lookupWithUpdatedTags()}
+     * @param consumer              registers a reload listener with an id for debugging, common listener types
+     *                              include:
+     *                              <ul>
+     *                              <li>{@link net.minecraft.server.packs.resources.PreparableReloadListener}</li>
+     *                              <li>{@link net.minecraft.server.packs.resources.ResourceManagerReloadListener}</li>
+     *                              <li>{@link net.minecraft.server.packs.resources.SimplePreparableReloadListener}</li>
+     *                              </ul>
      */
-    void onAddDataPackReloadListeners(BiConsumer<ResourceLocation, Function<HolderLookup.Provider, PreparableReloadListener>> consumer);
+    void onAddDataPackReloadListeners(RegistryAccess fullRegistries, HolderLookup.Provider lookupWithUpdatedTags, BiConsumer<ResourceLocation, PreparableReloadListener> consumer);
 }
--- Fabric/src/main/java/fuzs/puzzleslib/fabric/api/event/v1/FabricLifecycleEvents.java
+++ Fabric/src/main/java/fuzs/puzzleslib/fabric/api/event/v1/FabricLifecycleEvents.java
@@ -1,6 +1,7 @@
 package fuzs.puzzleslib.fabric.api.event.v1;
 
 import fuzs.puzzleslib.api.event.v1.LoadCompleteCallback;
+import fuzs.puzzleslib.api.event.v1.server.AddDataPackReloadListenersCallback;
 import fuzs.puzzleslib.fabric.api.event.v1.core.FabricEventFactory;
 import net.fabricmc.fabric.api.event.Event;
 
@@ -8,8 +9,12 @@ public final class FabricLifecycleEvents {
     /**
      * Fires when mod loading is complete.
      */
-    public static final Event<LoadCompleteCallback> LOAD_COMPLETE = FabricEventFactory.create(
-            LoadCompleteCallback.class);
+    public static final Event<LoadCompleteCallback> LOAD_COMPLETE = FabricEventFactory.create(LoadCompleteCallback.class);
+    /**
+     * Adds a listener to the server resource manager (for data packs) to reload at the end of all resources.
+     */
+    public static final Event<AddDataPackReloadListenersCallback> ADD_DATA_PACK_RELOAD_LISTENERS = FabricEventFactory.create(
+            AddDataPackReloadListenersCallback.class);
 
     private FabricLifecycleEvents() {
         // NO-OP
