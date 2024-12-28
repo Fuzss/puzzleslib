40226cd63288d2b29a50e8248bb1b27ef9f94a5d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-28 13:00:20 +0100

    split AbstractModelProvider helper methods

---
+++ 1.21.3/Common/src/main/java/fuzs/puzzleslib/api/client/data/v2/models/ModelLocationHelper.java
@@ -0,0 +1,65 @@
+package fuzs.puzzleslib.api.client.data.v2.models;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.models.model.ModelLocationUtils;
+import net.minecraft.data.models.model.TextureMapping;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public final class ModelLocationHelper {
+    public static final String BLOCK_PATH = "block";
+    public static final String ITEM_PATH = "item";
+
+    private ModelLocationHelper() {
+        // NO-OP
+    }
+
+    public static ResourceLocation getBlockModel(Block block) {
+        return ModelLocationUtils.getModelLocation(block);
+    }
+
+    public static ResourceLocation getBlockModel(ResourceLocation resourceLocation) {
+        return resourceLocation.withPrefix(BLOCK_PATH + "/");
+    }
+
+    public static ResourceLocation getBlockTexture(Block block) {
+        return TextureMapping.getBlockTexture(block);
+    }
+
+    public static ResourceLocation getBlockTexture(ResourceLocation resourceLocation) {
+        return resourceLocation.withPrefix(BLOCK_PATH + "/");
+    }
+
+    public static ResourceLocation getBlockLocation(Block block) {
+        return BuiltInRegistries.BLOCK.getKey(block);
+    }
+
+    public static String getBlockName(Block block) {
+        return getBlockLocation(block).getPath();
+    }
+
+    public static ResourceLocation getItemModel(Item item) {
+        return ModelLocationUtils.getModelLocation(item);
+    }
+
+    public static ResourceLocation getItemModel(ResourceLocation resourceLocation) {
+        return resourceLocation.withPrefix(ITEM_PATH + "/");
+    }
+
+    public static ResourceLocation getItemTexture(Item item) {
+        return TextureMapping.getItemTexture(item);
+    }
+
+    public static ResourceLocation getItemTexture(ResourceLocation resourceLocation) {
+        return resourceLocation.withPrefix(ITEM_PATH + "/");
+    }
+
+    public static ResourceLocation getItemLocation(Item item) {
+        return BuiltInRegistries.ITEM.getKey(item);
+    }
+
+    public static String getItemName(Item item) {
+        return getItemLocation(item).getPath();
+    }
+}
