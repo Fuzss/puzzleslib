ef597031d7b234cce9af579d48b4def6d7b82d23
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-07 06:31:33 +0200

    add ItemDataAccessor

--- 1.21.5/CHANGELOG.md
+++ 1.21.5/CHANGELOG.md
@@ -4,8 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v21.5.5-1.21.5] - 2025-04-05
+### Added
+- Add `ClientCommonHelper`
+
 ## [v21.5.4-1.21.5] - 2025-04-03
-### Add
+### Added
 - Add `HSV` utility class
 - Add one more `RegistryManager::registerSpawnEggItem` overload
 ### Fixed
---
+++ 1.21.5/Common/src/main/java/fuzs/puzzleslib/impl/content/ItemDataAccessor.java
@@ -0,0 +1,104 @@
+package fuzs.puzzleslib.impl.content;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import fuzs.puzzleslib.impl.PuzzlesLib;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.NbtPathArgument;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.server.commands.data.DataAccessor;
+import net.minecraft.server.commands.data.DataCommands;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Locale;
+import java.util.function.Function;
+
+/**
+ * @see net.minecraft.server.commands.data.EntityDataAccessor
+ */
+public class ItemDataAccessor implements DataAccessor {
+    private static final DynamicCommandExceptionType ERROR_NOT_LIVING_ENTITY = new DynamicCommandExceptionType(entity -> Component.translatableEscape(
+            "commands.enchant.failed.entity",
+            entity));
+    private static final DynamicCommandExceptionType ERROR_NO_ITEM = new DynamicCommandExceptionType(entity -> Component.translatableEscape(
+            "commands.enchant.failed.itemless",
+            entity));
+    public static final Function<String, DataCommands.DataProvider> PROVIDER = (String argumentName) -> new DataCommands.DataProvider() {
+        @Override
+        public DataAccessor access(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+            Entity entity = EntityArgument.getEntity(context, argumentName);
+            if (!(entity instanceof LivingEntity livingEntity)) {
+                throw ERROR_NOT_LIVING_ENTITY.create(entity.getName().getString());
+            }
+            ItemStack itemStack = livingEntity.getMainHandItem();
+            if (itemStack.isEmpty()) {
+                throw ERROR_NO_ITEM.create(entity.getName().getString());
+            }
+            return new ItemDataAccessor(context.getSource().registryAccess(), itemStack);
+        }
+
+        @Override
+        public ArgumentBuilder<CommandSourceStack, ?> wrap(ArgumentBuilder<CommandSourceStack, ?> builder, Function<ArgumentBuilder<CommandSourceStack, ?>, ArgumentBuilder<CommandSourceStack, ?>> action) {
+            return builder.then(Commands.literal("item")
+                    .then(action.apply(Commands.argument(argumentName, EntityArgument.entity()))));
+        }
+    };
+    private final RegistryAccess registryAccess;
+    private final ItemStack itemStack;
+
+    public ItemDataAccessor(RegistryAccess registryAccess, ItemStack itemStack) {
+        this.registryAccess = registryAccess;
+        this.itemStack = itemStack;
+    }
+
+    @Override
+    public void setData(CompoundTag compoundTag) throws CommandSyntaxException {
+        RegistryOps<Tag> registryOps = this.registryAccess.createSerializationContext(NbtOps.INSTANCE);
+        DataComponentMap.CODEC.parse(registryOps, compoundTag)
+                .resultOrPartial()
+                .ifPresent(this.itemStack::applyComponents);
+    }
+
+    @Override
+    public CompoundTag getData() {
+        RegistryOps<Tag> registryOps = this.registryAccess.createSerializationContext(NbtOps.INSTANCE);
+        return DataComponentMap.CODEC.encodeStart(registryOps, this.itemStack.getComponents())
+                .resultOrPartial(PuzzlesLib.LOGGER::error)
+                .map((Tag tag) -> tag instanceof CompoundTag compoundTag ? compoundTag : null)
+                .orElseGet(CompoundTag::new);
+    }
+
+    @Override
+    public Component getModifiedSuccess() {
+        return Component.translatable("commands.data.entity.modified", this.itemStack.getDisplayName());
+    }
+
+    @Override
+    public Component getPrintSuccess(Tag tag) {
+        return Component.translatable("commands.data.entity.query",
+                this.itemStack.getDisplayName(),
+                NbtUtils.toPrettyComponent(tag));
+    }
+
+    @Override
+    public Component getPrintSuccess(NbtPathArgument.NbtPath path, double scale, int value) {
+        return Component.translatable("commands.data.entity.get",
+                path.asString(),
+                this.itemStack.getDisplayName(),
+                String.format(Locale.ROOT, "%.2f", scale),
+                value);
+    }
+}
