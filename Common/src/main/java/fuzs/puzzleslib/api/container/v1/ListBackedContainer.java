8014d5e520cb0b9d609fe7fe59bb47b0aaf87169
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-04-11 23:15:38 +0200

    add SimpleContainerImpl

--- Common/src/main/java/fuzs/puzzleslib/api/container/v1/ListBackedContainer.java
+++ Common/src/main/java/fuzs/puzzleslib/api/container/v1/ListBackedContainer.java
@@ -5,13 +5,12 @@ import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 
 /**
  * A simple {@link Container} implementation with only default methods and an item list getter.
  */
 @FunctionalInterface
-public interface ListBackedContainer extends Container {
+public interface ListBackedContainer extends SimpleContainerImpl {
 
     /**
      * Creates a container backed by the item list.
@@ -29,59 +28,12 @@ public interface ListBackedContainer extends Container {
 
     /**
      * Retrieves the item list backing this inventory.
-     * <p>
-     * Must not be called <code>getItems</code> as it clashes with {@link BaseContainerBlockEntity#getItems()} causing
-     * remapping issues.
      */
     NonNullList<ItemStack> getContainerItems();
 
     @Override
     default int getContainerSize() {
         return this.getContainerItems().size();
-    }
-
-    @Override
-    default boolean isEmpty() {
-        for (ItemStack stack : this.getContainerItems()) {
-            if (!stack.isEmpty()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    default ItemStack getItem(int slot) {
-        return slot >= 0 && slot < this.getContainerSize() ? this.getContainerItems().get(slot) : ItemStack.EMPTY;
-    }
-
-    @Override
-    default ItemStack removeItem(int slot, int count) {
-        ItemStack result = ContainerHelper.removeItem(this.getContainerItems(), slot, count);
-        if (!result.isEmpty()) this.setChanged();
-        return result;
-    }
-
-    @Override
-    default ItemStack removeItemNoUpdate(int slot) {
-        return ContainerHelper.takeItem(this.getContainerItems(), slot);
-    }
-
-    @Override
-    default void setItem(int slot, ItemStack stack) {
-        if (slot >= 0 && slot < this.getContainerSize()) {
-            this.getContainerItems().set(slot, stack);
-            if (!stack.isEmpty() && stack.getCount() > this.getMaxStackSize()) {
-                stack.setCount(this.getMaxStackSize());
-            }
-            this.setChanged();
-        }
-    }
-
-    @Override
-    default void clearContent() {
-        this.getContainerItems().clear();
-        this.setChanged();
     }
 
     @Override
@@ -92,5 +44,25 @@ public interface ListBackedContainer extends Container {
     @Override
     default boolean stillValid(Player player) {
         return true;
+    }
+
+    @Override
+    default ItemStack getContainerItem(int slot) {
+        return this.getContainerItems().get(slot);
+    }
+
+    @Override
+    default ItemStack removeContainerItem(int slot, int amount) {
+        return ContainerHelper.removeItem(this.getContainerItems(), slot, amount);
+    }
+
+    @Override
+    default ItemStack removeContainerItemNoUpdate(int slot) {
+        return ContainerHelper.takeItem(this.getContainerItems(), slot);
+    }
+
+    @Override
+    default void setContainerItem(int slot, ItemStack itemStack) {
+        this.getContainerItems().set(slot, itemStack);
     }
 }
