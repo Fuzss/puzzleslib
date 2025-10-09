efa5b2350c291ccfd650b4aa25f1bf133693d79a
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-09 17:24:03 +0200

    update neoforge

--- NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/client/core/context/SkullRenderersContextNeoForgeImpl.java
+++ NeoForge/src/main/java/fuzs/puzzleslib/neoforge/impl/client/core/context/SkullRenderersContextNeoForgeImpl.java
@@ -3,7 +3,6 @@ package fuzs.puzzleslib.neoforge.impl.client.core.context;
 import fuzs.puzzleslib.api.client.core.v1.context.SkullRenderersContext;
 import net.minecraft.client.model.SkullModelBase;
 import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.SkullBlock;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
@@ -18,7 +17,6 @@ public record SkullRenderersContextNeoForgeImpl(EntityRenderersEvent.CreateSkull
         Objects.requireNonNull(skullBlockType, "skull block type is null");
         Objects.requireNonNull(textureLocation, "texture location is null");
         Objects.requireNonNull(skullModelFactory, "skull model factory is null");
-        this.event.registerSkullModel(skullBlockType, skullModelFactory);
-        SkullBlockRenderer.SKIN_BY_TYPE.put(skullBlockType, textureLocation);
+        this.event.registerSkullModel(skullBlockType, skullModelFactory, textureLocation);
     }
 }
