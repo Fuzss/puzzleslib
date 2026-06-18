d3f276965e7dea3b18dd1346ed2837eeccec2072
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-18 11:35:36 +0200

    fix access transformer

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("fuzs.multiloader.multiloader-convention-plugins-common")
 }
 
+neoForge {
+    // This breaks creating game artifacts which are required for looking up access level changes.
+    // Since this project contains a lot of access level changes, it's better to have the setting like this.
+//    validateAccessTransformers.set(false)
+}
+
 dependencies {
     modCompileOnlyApi(sharedLibs.forgeconfigapiport.common)
 }
--- NeoForge/build.gradle.kts
+++ NeoForge/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("fuzs.multiloader.multiloader-convention-plugins-neoforge")
 }
 
+neoForge {
+    // This breaks creating game artifacts which are required for looking up access level changes.
+    // Since this project contains a lot of access level changes, it's better to have the setting like this.
+//    validateAccessTransformers.set(false)
+}
+
 multiloader {
     mixins {
         plugin.set("${project.group}.${project.packageName}.mixin.MixinConfigPluginNeoForgeImpl")
