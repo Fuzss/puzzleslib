6616736831973541b63cc7dc73512e8f552866d7
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-08-03 14:17:32 +0200

    simplify ItemComponentsContextFabricImpl

--- Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/context/ItemComponentsContextFabricImpl.java
+++ Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/context/ItemComponentsContextFabricImpl.java
@@ -3,18 +3,14 @@ package fuzs.puzzleslib.fabric.impl.core.context;
 import fuzs.puzzleslib.common.api.core.v1.context.ItemComponentsContext;
 import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.component.DataComponentGetter;
 import net.minecraft.core.component.DataComponentMap;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.item.Item;
-import org.jspecify.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public final class ItemComponentsContextFabricImpl implements ItemComponentsContext {
     private final List<Consumer<DefaultItemComponentEvents.ModifyContext>> itemComponentPatches = new ArrayList<>();
@@ -27,7 +23,7 @@ public final class ItemComponentsContextFabricImpl implements ItemComponentsCont
         this.itemComponentPatches.add((DefaultItemComponentEvents.ModifyContext context) -> {
             context.modify(item,
                     (DataComponentMap.Builder builder, HolderLookup.Provider lookupProvider, Item itemInstance) -> {
-                        initializer.run(new DataComponentGetterImpl(builder), builder, lookupProvider, itemInstance);
+                        initializer.run(builder::get, builder, lookupProvider, itemInstance);
                     });
         });
     }
@@ -40,7 +36,7 @@ public final class ItemComponentsContextFabricImpl implements ItemComponentsCont
         this.itemComponentPatches.add((DefaultItemComponentEvents.ModifyContext context) -> {
             context.modify(itemPredicate,
                     (DataComponentMap.Builder builder, HolderLookup.Provider lookupProvider, Item itemInstance) -> {
-                        initializer.run(new DataComponentGetterImpl(builder), builder, lookupProvider, itemInstance);
+                        initializer.run(builder::get, builder, lookupProvider, itemInstance);
                     });
         });
     }
@@ -52,18 +48,6 @@ public final class ItemComponentsContextFabricImpl implements ItemComponentsCont
                     itemComponentPatch.accept(context);
                 }
             });
-        }
-    }
-
-    private record DataComponentGetterImpl(DataComponentMap.Builder builder) implements DataComponentGetter {
-        private static final Supplier<?> DEFAULT_VALUE_FALLBACK_SUPPLIER = () -> {
-            throw new IllegalStateException("Cannot invoke fallback when type is present");
-        };
-
-        @Override
-        public @Nullable <T> T get(DataComponentType<? extends T> type) {
-            return this.builder.contains(type) ? this.builder.getOrCreate((DataComponentType<T>) type,
-                    (Supplier<T>) DEFAULT_VALUE_FALLBACK_SUPPLIER) : null;
         }
     }
 }
