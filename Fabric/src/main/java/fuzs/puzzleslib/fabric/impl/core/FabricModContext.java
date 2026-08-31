defc2943e305a8d54eb76e3f35a2bf02b5a8152e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-08-31 14:49:19 +0200

    defer registering handshake payloads

--- Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/FabricModContext.java
+++ Fabric/src/main/java/fuzs/puzzleslib/fabric/impl/core/FabricModContext.java
@@ -1,16 +1,17 @@
 package fuzs.puzzleslib.fabric.impl.core;
 
-import fuzs.puzzleslib.fabric.impl.config.FabricConfigHolderImpl;
-import fuzs.puzzleslib.fabric.impl.init.FabricRegistryManager;
 import fuzs.puzzleslib.common.impl.config.ConfigHolderImpl;
 import fuzs.puzzleslib.common.impl.core.ModContext;
 import fuzs.puzzleslib.common.impl.init.RegistryManagerImpl;
+import fuzs.puzzleslib.fabric.impl.config.FabricConfigHolderImpl;
+import fuzs.puzzleslib.fabric.impl.init.FabricRegistryManager;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.common.custom.BrandPayload;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
 
 import java.util.Objects;
@@ -19,21 +20,25 @@ public final class FabricModContext extends ModContext {
 
     public FabricModContext(String modId) {
         super(modId);
-        PayloadTypeRegistry.serverboundPlay().register(this.payloadType, BrandPayload.STREAM_CODEC);
-        PayloadTypeRegistry.clientboundPlay().register(this.payloadType, BrandPayload.STREAM_CODEC);
-        FabricProxy.get().setupHandshakePayload(this.payloadType);
     }
 
     @Override
-    public boolean isPresentServerside() {
+    protected void setupHandshakePayload(String modId, CustomPacketPayload.Type<BrandPayload> payloadType) {
+        PayloadTypeRegistry.serverboundPlay().register(payloadType, BrandPayload.STREAM_CODEC);
+        PayloadTypeRegistry.clientboundPlay().register(payloadType, BrandPayload.STREAM_CODEC);
+        FabricProxy.get().setupHandshakePayload(payloadType);
+    }
+
+    @Override
+    protected boolean isPresentServerside(CustomPacketPayload.Type<BrandPayload> payloadType) {
         ClientPacketListener clientPacketListener = Minecraft.getInstance().getConnection();
-        return clientPacketListener != null && ClientPlayNetworking.canSend(this.payloadType);
+        return clientPacketListener != null && ClientPlayNetworking.canSend(payloadType);
     }
 
     @Override
-    public boolean isPresentClientside(ServerPlayer serverPlayer) {
+    protected boolean isPresentClientside(CustomPacketPayload.Type<BrandPayload> payloadType, ServerPlayer serverPlayer) {
         Objects.requireNonNull(serverPlayer, "server player is null");
-        return ServerPlayNetworking.canSend(serverPlayer.connection, this.payloadType);
+        return ServerPlayNetworking.canSend(serverPlayer.connection, payloadType);
     }
 
     @Override
